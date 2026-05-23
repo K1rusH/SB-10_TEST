decfbd9e70cfd7575151d1678162fdd025a181ff
trofan03 <vormaks447@gmail.com>
2026-05-23 15:22:54 +0300

    Update new_password.py

--- password/new_password.py
+++ password/new_password.py
@@ -8,6 +8,11 @@ def generate_password(length=12):
     for i in range(length):
         password += random.choice(characters)
     return password
+def test_different_passwords():
+    password1 = generate_password()
+    password2 = generate_password()
+    assert password1 != password2
+
 
 # Пример использования
 password_length = 12  # Вы можете выбрать любую длину пароля
